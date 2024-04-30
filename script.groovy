f32d8e6dd8377c1dfef0eaac793852b9dd285360
ashok <karishma98@gmail.com>
2024-05-01 01:57:56 +0530

    eighth commit - Build application and then docker image and push to DockerHub with steps in script.groovy file

--- script.groovy
+++ script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
 } 
 
-def testApp() {
-    echo 'testing the application...'
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker image build -t navr24396/java-maven-app:jma-2.1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push navr24396/java-maven-app:jma-2.1'
+    }
 } 
 
 def deployApp() {
-    echo 'deplying the application...'
-    echo "deploying version ${params.VERSION}"
+    echo 'deploying the application...'
 } 
 
 return this
